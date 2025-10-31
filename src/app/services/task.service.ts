82667a31747b02d0a5e55183d3d7bac23255fbeb
Aluizio Developer <aluiziodeveloper@gmail.com>
2025-10-31 14:16:54 -0300

    Criando lógica de atualização de comentários no TaskService

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
+import { IComment } from '../interfaces/comment.interface';
 import { TaskStatusEnum } from '../enums/task-status.enum';
 import { ITaskFormControls } from '../interfaces/task.form-controls.interface';
 import { ITask } from '../interfaces/task.interface';
@@ -83,6 +84,28 @@ export class TaskService {
         name: newTaskName,
         description: newTaskDescription,
       };
+      currentTaskList.next(updatedTaskList);
+    }
+  }
+
+  updateTaskComments(
+    taskId: string,
+    taskCurrentStatus: TaskStatus,
+    newTaskComments: IComment[],
+  ) {
+    const currentTaskList = this.getTaskListByStatus(taskCurrentStatus);
+    const currentTaskIndex = currentTaskList.value.findIndex(
+      (task) => task.id === taskId,
+    );
+
+    if (currentTaskIndex > -1) {
+      const updatedTaskList = [...currentTaskList.value];
+
+      updatedTaskList[currentTaskIndex] = {
+        ...updatedTaskList[currentTaskIndex],
+        comments: [...newTaskComments],
+      };
+
       currentTaskList.next(updatedTaskList);
     }
   }
